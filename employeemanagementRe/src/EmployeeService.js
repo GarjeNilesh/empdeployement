dc38d255abc0ee67b95a2a920fb55444156467f0
Nilesh Garje <garjenilesh5858@gmail.com>
2026-08-13 22:59:10 +0530

    url Fixed

--- employeemanagementRe/src/EmployeeService.js
+++ employeemanagementRe/src/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080";
+const API_URL = "https://empdeployement-1.onrender.com";
 
 class EmployeeService {
   saveEmployee(employee) {
